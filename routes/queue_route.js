a6e55900fd34fc3dafaf20fa2f9da123b544a41e
wuzengfu <wuzengfu666@gmail.com>
2021-05-05 21:36:28 +0800

    getQueue route added

--- routes/queue_route.js
+++ routes/queue_route.js
@@ -3,18 +3,23 @@ const queueManager = require('../managers/queue_manager');
 
 const router = express.Router();
 
-router.post('/', (req, res, next) =>
-    queueManager
-        .enqueue()
-        .then((response) => res.status(201).json(response))
-        .catch(next),
-);
+router.post('/', (req, res, next) => queueManager
+    .enqueue()
+    .then((response) => res.status(201)
+        .json(response))
+    .catch(next),);
 
-router.delete('/', (req, res, next) =>
-    queueManager
-        .dequeue()
-        .then((response) => res.json(response))
-        .catch(next),
-);
+router.delete('/', (req, res, next) => queueManager
+    .dequeue()
+    .then((response) => res.json(response))
+    .catch(next),);
+
+//feature 2: GET /queue
+router.get('/', ((req, res, next) => {
+    queueManager.getQueue()
+        .then((response) => res.status(200)
+            .json(response))
+        .catch(next,);
+}));
 
 module.exports = router;
