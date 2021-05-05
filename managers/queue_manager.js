0653e1a1679fa7f122f26f9b2d910b7b4d607de3
wuzengfu <wuzengfu666@gmail.com>
2021-05-05 21:36:09 +0800

    getQueue middleware added

--- managers/queue_manager.js
+++ managers/queue_manager.js
@@ -1,9 +1,16 @@
 const dbManager = require('./db_manager');
 
 module.exports.enqueue = function () {
-    return dbManager.enqueue().then((customerId) => ({ customer_id: customerId }));
+    return dbManager.enqueue()
+        .then((customerId) => ({ customer_id: customerId }));
 };
 
 module.exports.dequeue = function () {
-    return dbManager.dequeue().then((customerId) => ({ customer_id: customerId }));
+    return dbManager.dequeue()
+        .then((customerId) => ({ customer_id: customerId }));
+};
+
+//feature 2: GET /queue
+module.exports.getQueue = function () {
+    return dbManager.getQueue();
 };
