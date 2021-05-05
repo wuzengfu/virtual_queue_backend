eb63057c223d212d0a1c5260f49f6906e1c55276
wuzengfu <wuzengfu666@gmail.com>
2021-05-05 21:37:55 +0800

    getArrivals route added

--- routes/stats_route.js
+++ routes/stats_route.js
@@ -3,8 +3,25 @@ const statsManager = require('../managers/stats_manager');
 
 const router = express.Router();
 
+//feature 1: GET /stats/arrivals
+router.get('/arrivals', (req, res, next) => {
+    const {
+        from,
+        duration
+    } = req.query;
+
+    return statsManager.getArrivals(from, duration)
+        .then((response) => res.status(200)
+            .json(response))
+        .catch(next);
+});
+
 router.get('/errors', (req, res, next) => {
-    const { from, duration, status_codes: statusCodes } = req.query;
+    const {
+        from,
+        duration,
+        status_codes: statusCodes
+    } = req.query;
     return statsManager
         .getErrors(from, duration, statusCodes)
         .then((response) => res.json(response))
