a288ad1d1d107d65ef5db0f977078276c8f29736
wuzengfu <wuzengfu666@gmail.com>
2021-05-07 18:23:08 +0800

    rename object key name sent from db_manager.js

--- managers/queue_manager.js
+++ managers/queue_manager.js
@@ -12,5 +12,18 @@ module.exports.dequeue = function () {
 
 //feature 2: GET /queue
 module.exports.getQueue = function () {
-    return dbManager.getQueue();
+    return dbManager.getQueue()
+        .then(rows => {
+            /**
+             * Rename object key from db_manager
+             * id => customer_id
+             * arrival_timestamp => arrival
+             */
+            return rows.map(item => {
+                return {
+                    customer_id: item['id'],
+                    arrival: item['arrival_timestamp']
+                };
+            });
+        });
 };
