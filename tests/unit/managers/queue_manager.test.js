bb9a69a9620e0ebebaeea304e9e0862218591f53
wuzengfu <wuzengfu666@gmail.com>
2021-05-12 17:07:48 +0800

    unit testing for getQueue added

--- tests/unit/managers/queue_manager.test.js
+++ tests/unit/managers/queue_manager.test.js
@@ -33,6 +33,57 @@ it('Should reject dequeue correctly', function () {
         .then(revertDbManagerDequeue);
 });
 
+//feature 2: Get /queue
+console.log("Getting current queue");
+const revertDbManagerGetQueue = makeRevertFunction(dbManager, 'getQueue');
+function testGetQueueSuccess(description, databaseResponseValue) {
+    it(description, function () {
+        const output = databaseResponseValue.map(row => {
+            return {
+                customer_id: row.id,
+                arrival: row.arrival_timestamp
+            }
+        });
+        dbManager.getQueue = function () {
+            return Promise.resolve(databaseResponseValue);
+        }
+        return queueManager.getQueue().
+        then((result) => JSON.stringify(result) === JSON.stringify(output))
+            .then(revertDbManagerGetQueue);
+    });
+}
+
+testGetQueueSuccess("getQueue should return empty array correctly",[]);
+testGetQueueSuccess("getQueue should return array with 1 entry correctly",[{
+    id: 5,
+    arrival_timestamp: 123
+}]);
+testGetQueueSuccess("getQueue should return array with multiple entry correctly",[
+    {
+        id: 5,
+        arrival_timestamp: 456
+    },
+    {
+        id: 6,
+        arrival_timestamp: 789
+    },
+    {
+        id: 10,
+        arrival_timestamp: 1234567
+    }
+])
+
+
+it('getQueue should reject with rejected value correctly', function (){
+    dbManager.getQueue = function () {
+        return Promise.reject("error!");
+    }
+    return queueManager.getQueue()
+        .then(() => false)
+        .catch(error => error === "error!")
+        .then(revertDbManagerGetQueue);
+})
+
 // Run the tests
 // Important: Keep this as the last line
 run();
